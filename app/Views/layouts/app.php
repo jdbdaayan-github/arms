bffacff041feffbe9e099f09a0112bc950ebd240
jdbdaayan-github <jdbdaayan@gmailcom>
2025-03-25 13:37:46 +0800

    move vendor content to assets in public folder

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -6,16 +6,16 @@
     <title>ARMS | Admin</title>
 
         <!-- FontAwesome - Load First -->
-    <link rel="stylesheet" href="<?= base_url('vendor/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>" crossorigin="anonymous" as="font">
+    <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>" crossorigin="anonymous" as="font">
 
     <!-- Main CSS -->
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css?v=' . time()); ?>">
 
     <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="<?= base_url('vendor/adminlte/dist/css/adminlte.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css'); ?>">
 
     <!-- DataTables -->
-    <link rel="stylesheet" href="<?= base_url('vendor/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css'); ?>">
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -39,17 +39,17 @@
     </div>
 
     <!-- jQuery -->
-    <script src="<?= base_url('vendor/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js'); ?>"></script>
 
     <!-- Bootstrap JS -->
-    <script src="<?= base_url('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
     <!-- AdminLTE JS -->
-    <script src="<?= base_url('vendor/adminlte/dist/js/adminlte.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js'); ?>"></script>
 
     <!-- DataTables Script -->
-    <script src="<?= base_url('vendor/adminlte/plugins/datatables/jquery.dataTables.min.js'); ?>"></script>
-    <script src="<?= base_url('vendor/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/datatables/jquery.dataTables.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js'); ?>"></script>
     <script src="<?= base_url('assets/js/scripts.js') ?>"></script>
     <script>
     
