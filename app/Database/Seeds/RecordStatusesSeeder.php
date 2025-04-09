1ed66df992cc806fb8b87ba60d6f9c3b8a827a89
June Delrey B. Da-ayan <jdbdaayan@dswd.gov.ph>
2025-04-09 16:26:47 +0800

    Remove unnecessary files, create controller and directories

--- app/Database/Seeds/RecordStatusesSeeder.php
+++ app/Database/Seeds/RecordStatusesSeeder.php
@@ -8,13 +8,6 @@ class RecordStatusesSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            ['id' => 1, 'name' => 'Pending'],
-            ['id' => 2, 'name' => 'Active'],
-            ['id' => 3, 'name' => 'Inactive'],
-            ['id' => 4, 'name' => 'Banned'],
-            ['id' => 5, 'name' => 'Archived'],
-        ];
-        $this->db->table('statuses')->insertBatch($data);
+        //
     }
 }
--- app/Database/Seeds/UserStatusesSeeder.php
+++ app/Database/Seeds/UserStatusesSeeder.php
@@ -8,6 +8,13 @@ class UserStatusesSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $data = [
+            ['id' => 1, 'name' => 'Pending'],
+            ['id' => 2, 'name' => 'Active'],
+            ['id' => 3, 'name' => 'Inactive'],
+            ['id' => 4, 'name' => 'Banned'],
+            ['id' => 5, 'name' => 'Archived'],
+        ];
+        $this->db->table('user_statuses')->insertBatch($data);
     }
 }
